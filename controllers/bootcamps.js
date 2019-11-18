cf72fe7c1f35f53397130ca094ff57b880c6cb03
Nikita <nik.starch@gmail.com>
2019-11-17 19:22:04 -0500

    Fourth Submit

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const Bootcamp = require("../models/Bootcamp");
 // @desc Get all bootcamps
 // @route GET /api/v1/bootcamps
 // @access Public
@@ -15,8 +16,19 @@ exports.getBootcamp = (req, res, next) => {
 // @desc Create new bootcamp
 // @route POST /api/v1/bootcamps
 // @access Private
-exports.createBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false
+    });
+  }
 };
 
 // @desc Update a bootcamp
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
-//Route files
-const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Connect DB
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+//Body Parser
+app.use(express.json());
 
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -17,8 +26,17 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   );
+});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+  //Close server & exit process
+  server.close(() => process.exit(1));
 });
